3953d9279749c9f8593c112d59fc15c017fa94d4
wanghuipeng <wanghuipeng@youlanw.com>
2019-07-12 10:10:46 +0800

    设置端口号和代理

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,20 @@
 module.exports = {
     devServer: {
-        //proxy: 'http://127.0.0.1:8080/static'
+        // proxy: {
+        //     '/api': {
+        //         target: 'http://www.example.org',
+        //         changeOrigin: true,
+        //         ws: true,
+        //         pathRewrite: {
+        //           '^/api': ''
+        //         }
+        //     }
+        // }
     },
     publicPath: process.env.NODE_ENV === 'production' ?
         '/vue-ts/' : '/',
-    outputDir: 'vue-ts'
+    outputDir: 'vue-ts',
+    devServer: {
+        port: '3030'
+    }
 }
